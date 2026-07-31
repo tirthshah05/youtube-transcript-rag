a90114c5a8b8de5ed03fa6da34d4f57dae65c133
Tirth Shah <220359733+tirthshah05@users.noreply.github.com>
2026-07-31 17:21:58 +0530

    Update chunking for Supadata transcript

--- chunking.py
+++ chunking.py
@@ -1,21 +1,15 @@
 def create_chunks(transcript):
+    words = transcript.split()
+
     chunks = []
 
-    chunk_size = 20
-    overlap = 5
+    chunk_size = 200
+    overlap = 50
     step = chunk_size - overlap
 
-    for i in range(0, len(transcript), step):
-
-        current_chunk = transcript[i:i + chunk_size]
-
-        text = []
-
-        for snippet in current_chunk:
-            text.append(snippet.text)
-
-        chunk = "\n".join(text)
-
+    for i in range(0, len(words), step):
+        current_chunk = words[i:i + chunk_size]
+        chunk = " ".join(current_chunk)
         chunks.append(chunk)
 
     return chunks
